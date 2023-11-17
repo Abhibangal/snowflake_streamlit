363105e1056f9ecbfb4be29ac627369795999504
Abhijit Bangal <118591207+Abhibangal@users.noreply.github.com>
2023-11-17 18:34:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(layout='wide', page_title='SNOWFLAKE_DATA_APP')
 st.title('Ordershorder')
 st.header("Healthy Food Deliver's")
-st.subheader('Veg/🍗Non-Veg')
+st.subheader('🥑Veg/🐔Non-Veg')
 st.text('🥦Kith n Kin')
 st.text('Modern Cafe')
 st.text('Shalu')
