9b0be4d81d2c891c7d2dcbfba05b74f2beb03538
Abhijit Bangal <118591207+Abhibangal@users.noreply.github.com>
2023-11-17 18:40:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-st.dataframe(fruit_list)
 st.set_page_config(layout='wide', page_title='SNOWFLAKE_DATA_APP')
 st.title('Ordershorder')
 st.header("Healthy Food Deliver's")
@@ -10,3 +8,6 @@ st.text('🥦Kith n Kin')
 st.text('Modern Cafe')
 st.text('Shalu')
 st.text('G2')
+
+fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+st.dataframe(fruit_list)
